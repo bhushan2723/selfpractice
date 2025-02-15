c2397e60d4bad390211b97d40d5a85e897e39d24
Bhushan Gyaniram Patil <bhushu2723@gmail.com>
2025-02-15 23:21:23 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,24 @@
+// Java program to reverse a number 
+import java.io.*; 
+
+class GFG { 
+	static int reverse(int n) 
+	{ 	
+		int rev = 0; 
+		int rem; 
+
+		while (n > 0) { 
+			rem = n % 10; 
+			rev = (rev * 10) + rem; 
+			n = n / 10; 
+		} 
+		return rev; 
+	} 
+	public static void main(String[] args) 
+	{ 
+		int n = 4526; 
+
+		System.out.print("Reversed Number is "
+						+ reverse(n)); 
+	} 
+}
